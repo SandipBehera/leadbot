248af9195bd4037b19d0fedcb35d893bc76bf6e9
SandipBehera <sandip.behera@cloudmargin.com>
2023-07-18 21:52:22 +0530

    added databse

--- actions/actions.py
+++ actions/actions.py
@@ -25,11 +25,16 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
-from typing import Any, Text, Dict, List
-from rasa_sdk import Action, Tracker
-from rasa_sdk.executor import CollectingDispatcher
+
 import logging
 import sys
+from typing import Any, Dict, List, Text
+
+# from database_pg import storeData
+import psycopg2
+from rasa_sdk import Action, Tracker
+from rasa_sdk.events import SlotSet
+from rasa_sdk.executor import CollectingDispatcher
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -45,9 +50,10 @@ file_handler.setFormatter(formatter)
 logger.addHandler(stdout_handler)
 logger.addHandler(file_handler)
 
+
 class ActionSubmitInfo(Action):
     def name(self) -> Text:
-        return "utter_say_thank_you"
+        return "action_submit_info"
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
@@ -55,16 +61,40 @@ class ActionSubmitInfo(Action):
         name = tracker.get_slot("user_name")
         email = tracker.get_slot("email")
         phone_number = tracker.get_slot("phone")
-        
+
         logger.info("Name slot value:", name)
         logger.info("Email slot value:", email)
         logger.info("Phone slot value:", phone_number)
         # Perform actions with the collected information
         # For example, you can store it in a database or perform further processing
+        # storeData(name, email, phone_number)/
+        try:
+            conn = psycopg2.connect(
+                host="localhost",
+                port="5432",
+                dbname="your_database",
+                user="your_username",
+                password="your_password"
+            )
 
-        dispatcher.utter_message(text=f"Thank you, {name} for providing your information!")
+            # Create a cursor object to execute SQL queries
+            cur = conn.cursor()
 
-        return "action_submit_info"
+            # Execute an SQL INSERT statement to store the data
+            insert_query = "INSERT INTO public.chat_bot_lead(name, email, phone) VALUES (%s, %s, %s)"
+            # Provide the actual data for insertion
+            data = (name, email, phone_number)
+            cur.execute(insert_query, data)
+            logger.info("Data inserted successfully", data)
+
+            # Commit the transaction and close the connection
+            conn.commit()
+        except Exception as e:
+            logger.error(e)
+        dispatcher.utter_message(
+            text=f"Thank you, {name} for providing your information!")
+
+        return [SlotSet("user_name", None), SlotSet("email", None), SlotSet("phone", None)]
 
 # class ActionDefaultResponse(Action):
 #     def name(self) -> Text:
@@ -73,6 +103,6 @@ class ActionSubmitInfo(Action):
 #     def run(self, dispatcher: CollectingDispatcher,
 #             tracker: Tracker,
 #             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        
+
 #         dispatcher.utter_message(response="utter_default_response")
 #         return []
